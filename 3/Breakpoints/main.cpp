da55d282f34705b6930c11950234af2519fd3e22
Kahoneki <shinyzorua6@gmail.com>
2023-08-03 19:14:00 +0100

    3 - Debugging

---
+++ 3/Breakpoints/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    int num{};
+
+    std::cout << "Enter a integer: ";
+    std::cout << "Enter a integer: ";
+    std::cout << "Enter a integer: ";
+    std::cout << "Enter a integer: ";
+    std::cout << "Enter a integer: ";
+    std::cout << "Enter a integer: ";
+    std::cout << "Enter a integer: ";
+    
+    std::cin >> num;
+    std::cout << "Double that number is " << num*2;
+}
