231f77a853c10795cb71ce4463400ab4c3aee191
Kahoneki <shinyzorua6@gmail.com>
2023-08-09 20:56:28 +0100

    O.2 - Bitwise Operators V2

--- O/O.2 - Bitwise Operators/LeftRotate2.cpp
+++ O/O.2 - Bitwise Operators/LeftRotate2.cpp
@@ -7,12 +7,23 @@ std::bitset<4> rotl(std::bitset<4> bits) {
 }
 
 
+std::bitset<4> rotr(std::bitset<4> bits) {
+    return (bits>>1) | (bits<<3);
+}
+
+
 int main() {
     std::bitset<4> bits1{0b0001};
     std::cout << rotl(bits1) << '\n';
 
     std::bitset<4> bits2{0b1001};
     std::cout << rotl(bits2) << '\n';
+
+    std::bitset<4> bits3{0b0001};
+    std::cout << rotr(bits1) << '\n';
+
+    std::bitset<4> bits4{0b1001};
+    std::cout << rotr(bits2) << '\n';
 
     return 0;
 }
